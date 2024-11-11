4bcd37d7bc84d53c529d0d8b6df7bc78db268066
yxj52 <1028530975@qq.com>
2024-11-11 19:10:17 +0800

    fix(初始化界面):

---
+++ controllers/login_controller.py
@@ -0,0 +1,22 @@
+import flet as ft
+from views.login_view import LoginView
+
+
+class LoginController:
+    def __init__(self, page: ft.Page, app):   
+        self.page = page
+        self.app = app
+        self.view = LoginView(page, self.handle_login, self.go_to_signup, self.app.go_back)
+
+    def handle_login(self, username, password):
+       if username == "" or password == "":
+        print("请输入账号或密码")
+       
+    def go_to_signup(self):
+        self.app.navigate("/signup")
+        print("Você foi pra tela Signup")
+
+    def on_back(self, event):
+        # Logic to navigate back
+        self.app.navigate()
+        print("voce clicou para voltar")
---
+++ views/signup_view.py
@@ -0,0 +1,94 @@
+import flet as ft
+from utils.utils import create_app_bar
+from utils.utils import msg_erro
+
+class SignUpView:
+    def __init__(self, page: ft.Page, on_signup, on_back):
+        self.page = page
+        self.on_signup = on_signup
+        self.on_back = on_back
+        self.setup_page()
+        self.signup_page_ui()
+        self.page.add(create_app_bar("Sign Up", on_back))
+
+
+    def setup_page(self):
+        self.page.bgcolor = ft.colors.WHITE
+        self.page.window_width = 350
+        self.page.window_height = 600
+        self.page.window_resizable = True
+        self.page.window_always_on_top = True
+        self.page.title = 'Signup'
+        self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def signup_page_ui(self):
+        self.page.controls.clear() 
+        
+        lb_cadastro = ft.Text(
+        "Cadastro de usuário",
+        size=12,
+        color=ft.colors.BLACK,
+        weight=ft.FontWeight.BOLD,
+        )
+
+        tf_firstname = ft.TextField(
+            height=40,
+            text_size=12,
+            color=ft.colors.BLACK,
+            label="Nome",
+        )
+        tf_lastname = ft.TextField( 
+            height=40,
+            text_size=12,
+            label='Sobrenome',
+            color=ft.colors.BLACK,
+        )
+        tf_email = ft.TextField(
+            height=40,
+            text_size=12,
+            label='E-mail',
+            color=ft.colors.BLACK,
+        )
+        tf_telefone = ft.TextField(
+            label='Telefone',
+            height=40,
+            text_size=12,
+            color=ft.colors.BLACK,
+        )
+        tf_password = ft.TextField(
+            label='Crie sua senha',
+            height=40,
+            text_size=12,
+            color=ft.colors.BLACK,
+            password=True,
+            can_reveal_password=True
+        )
+        tf_confirmpassword = ft.TextField(
+            label='Digite sua senha novamente',
+            height=40,
+            text_size=12,
+            color=ft.colors.BLACK,
+            password=True,
+            can_reveal_password=True
+        )
+
+        bt_signup = ft.ElevatedButton(
+            "Sign Up",
+            on_click=lambda e: self.on_signup(tf_firstname.value, tf_lastname.value, tf_email.value,
+                                              tf_telefone.value, tf_password.value, tf_confirmpassword.value),
+            width=120,
+            height=40
+        )
+
+
+        self.page.add(
+                      lb_cadastro, 
+                      tf_firstname, 
+                      tf_lastname, 
+                      tf_email, 
+                      tf_telefone, 
+                      tf_password, 
+                      tf_confirmpassword,
+                      bt_signup)
+        self.page.update()
