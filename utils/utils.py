8b64178cd481da8d44001e5669fcd4abee2466b7
yxj52 <1028530975@qq.com>
2024-11-11 19:10:17 +0800

    fix(初始化界面):

---
+++ utils/utils.py
@@ -0,0 +1,32 @@
+import flet as ft
+
+
+def create_app_bar(title, go_back):
+    bt_back = ft.IconButton(
+            icon=ft.icons.ARROW_BACK,
+            on_click=go_back,
+            width=120,
+            height=50
+     )
+    
+
+    return ft.AppBar(
+        title=ft.Text(title),
+        leading=bt_back
+    )
+
+def msg_erro(msg, open):
+    banner = ft.Banner(
+        open=open,
+        bgcolor=ft.colors.AMBER_100,
+        leading=ft.Icon(
+            ft.icons.DANGEROUS_SHARP,
+            color=ft.colors.RED_400,
+            size=40
+            ),
+        content=ft.Text(msg,
+                        color=ft.colors.BLACK,
+                        size=10
+                ),
+        )
+    return banner
---
+++ views/login_view.py
@@ -0,0 +1,69 @@
+import flet as ft
+from utils.utils import create_app_bar
+
+class LoginView:
+    def __init__(self, page: ft.Page, on_login, on_signup, go_back):
+        self.page = page
+        self.on_login = on_login
+        self.on_signup = on_signup
+        self.on_back = go_back
+        self.setup_page()
+        self.login_page_ui()
+        self.app_bar = create_app_bar("Login", go_back)
+        #print(create_app_bar("Login", on_back))
+        self.page.add(self.app_bar)
+
+    def setup_page(self):
+        self.page.controls.clear()
+        self.page.bgcolor = ft.colors.WHITE
+        self.page.window_width = 350
+        self.page.window_height = 600
+        self.page.window_resizable = False
+        self.page.window_always_on_top = True
+        self.page.title = 'Login'
+        self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER  
+        self.page.update()
+        
+    def login_page_ui(self):
+        lb_login = ft.Text(
+            "Autenticação de usuário",
+            size=12,
+            color=ft.colors.BLACK,
+            weight=ft.FontWeight.BOLD,
+        )
+
+        tf_usuario = ft.TextField(
+            label='Usuário',
+            hint_text='Usuário',
+            height=40,
+            text_size=12,
+            color=ft.colors.BLACK,
+        )
+
+        tf_senha = ft.TextField(
+            label='Senha',
+            hint_text='Senha',
+            text_size=12,
+            height=40,
+            color=ft.colors.BLACK,
+            password=True,
+            can_reveal_password=True
+        )
+
+        bt_login = ft.ElevatedButton(
+            "Login",
+            on_click=lambda e: self.on_login(tf_usuario.value, tf_senha.value),
+            width=100,
+            height=40,
+        )
+
+        bt_criar = ft.TextButton(
+            "Cadastrar-se",
+            on_click=lambda _: self.on_signup(),
+            width=120,
+            height=40
+        )
+
+        self.page.add(lb_login, tf_usuario, tf_senha, bt_login, bt_criar)
+        self.page.update()
